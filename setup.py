712814ce636e08e8a278a213ebc9f5aab472fd86
imrankhan-ik <imran.khan@zyte.com>
2023-12-25 16:26:08 +0500

    specified version

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="scrapy-dropbox",
     packages=find_packages(),
+    version='0.2',
     install_requires=["scrapy", "dropbox"],
     requires=["scrapy", "dropbox"],
     setup_requires=["wheel"],
